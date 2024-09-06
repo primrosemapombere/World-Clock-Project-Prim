fa2a2c7a8b6179324ec3d051f300238284db8a28
primrosemapombere <primrosemapombere@gmail.com>
2024-09-06 11:53:43 +0200

    updated html and js to suit local location and time zone and added links to netlify and github

--- src/index.js
+++ src/index.js
@@ -1,26 +1,26 @@
 function updateTime() {
-  // Los Angeles
-  let losAngelesElement = document.querySelector("#los-angeles");
-  if (losAngelesElement) {
-    let losAngelesDateElement = losAngelesElement.querySelector(".date");
-    let losAngelesTimeElement = losAngelesElement.querySelector(".time");
-    let losAngelesTime = moment().tz("America/Los_Angeles");
+  // Seoul
+  let SeoulElement = document.querySelector("#Seoul");
+  if (SeoulElement) {
+    let SeoulDateElement = SeoulElement.querySelector(".date");
+    let SeoulTimeElement = SeoulElement.querySelector(".time");
+    let SeoulTime = moment().tz("Asia/Seoul");
 
-    losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM	Do YYYY");
-    losAngelesTimeElement.innerHTML = losAngelesTime.format(
+    SeoulDateElement.innerHTML = SeoulTime.format("MMMM	Do YYYY");
+    SeoulTimeElement.innerHTML = SeoulTime.format(
       "h:mm:ss [<small>]A[</small>]"
     );
   }
 
-  // Paris
-  let parisElement = document.querySelector("#paris");
-  if (parisElement) {
-    let parisDateElement = parisElement.querySelector(".date");
-    let parisTimeElement = parisElement.querySelector(".time");
-    let parisTime = moment().tz("Europe/Paris");
+  // Victoria
+  let VictoriaElement = document.querySelector("#Victoria");
+  if (VictoriaElement) {
+    let VictoriaDateElement = VictoriaElement.querySelector(".date");
+    let VictoriaTimeElement = VictoriaElement.querySelector(".time");
+    let VictoriaTime = moment().tz("Australia/Victoria");
 
-    parisDateElement.innerHTML = parisTime.format("MMMM	Do YYYY");
-    parisTimeElement.innerHTML = parisTime.format(
+    VictoriaDateElement.innerHTML = VictoriaTime.format("MMMM	Do YYYY");
+    VictoriaTimeElement.innerHTML = VictoriaTime.format(
       "h:mm:ss [<small>]A[</small>]"
     );
   }
